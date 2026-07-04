dede9ab1a3698aba948ecd1c1dc89bd19ca9a22c
MohammadMahdi TorkTatari <146365848+MAINMMTTMAIN@users.noreply.github.com>
2026-07-04 19:55:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def resource_path(relative_path):
         # وقتی برنامه به exe تبدیل می‌شود، فایل‌ها در اینجا قرار می‌گیرند
         base_path = sys._MEIPASS
     except Exception:
-        base_path = os.path.abspath(".")
+        base_path = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base_path, relative_path)
 
 def get_base_path():
